2d5926fb7b412071dba8b713c97f9e6327590ea6
suranjan rai <suranjanthulungrai@gmail.com>
2020-08-01 16:01:01 +0545

    Updates

---
+++ precache-manifest.98fa6426bd4dc4b3a6472985b9b4a9d9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "384e077d2a035635532e1b49b0eaf8d4",
+    "url": "/Rezume/index.html"
+  },
+  {
+    "revision": "2e48c7e883b69ad71ac4",
+    "url": "/Rezume/static/css/main.a024a2b4.chunk.css"
+  },
+  {
+    "revision": "84a13d4920db4540cfa7",
+    "url": "/Rezume/static/js/2.ce9de395.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Rezume/static/js/2.ce9de395.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e48c7e883b69ad71ac4",
+    "url": "/Rezume/static/js/main.d9d4ca9b.chunk.js"
+  },
+  {
+    "revision": "2d520590580771f3ccb9",
+    "url": "/Rezume/static/js/runtime-main.24288ace.js"
+  },
+  {
+    "revision": "5837f08fb6d25231d2ecf6ca7b73d4b4",
+    "url": "/Rezume/static/media/UXUI.5837f08f.png"
+  },
+  {
+    "revision": "c4d4583e328d5a72a4878eab33b07988",
+    "url": "/Rezume/static/media/html.c4d4583e.jpg"
+  },
+  {
+    "revision": "f6146dc7e02e3ed50fba973288b48ffc",
+    "url": "/Rezume/static/media/ps.f6146dc7.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Rezume/precache-manifest.34f868c7821a98f1caf28c81aff09869.js"
+  "/Rezume/precache-manifest.98fa6426bd4dc4b3a6472985b9b4a9d9.js"
 );
 
 self.addEventListener('message', (event) => {
