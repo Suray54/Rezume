cc557254b424c4db7e6da005b9d1fbd4828dd631
Suranjan Rai <Suranjanthulungrai@gmail.com>
2020-10-12 00:36:27 +0545

    last

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Homepage from "./pages/homepage/homepage.component";
 import Output from "./pages/output/output.component";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import "./chat"
+
 import SimpleReactLightbox from "simple-react-lightbox";
 export default class App extends Component {
   render() {
